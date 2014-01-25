39482f9aa2df972130295de6d809f5059d387853
JuhaniImberg <juhani@imberg.fi>
2014-01-25 17:45:59 +0200

    ability to run a test from csv

--- src/main/java/fi/paivola/mapserver/App.java
+++ src/main/java/fi/paivola/mapserver/App.java
@@ -4,11 +4,16 @@ import fi.paivola.mapserver.core.GameManager;
 import fi.paivola.mapserver.core.GameThread;
 import fi.paivola.mapserver.core.Model;
 import fi.paivola.mapserver.core.SettingsParser;
+import fi.paivola.mapserver.core.TestcaseRunner;
 import fi.paivola.mapserver.core.WSServer;
 import fi.paivola.mapserver.core.setting.SettingMaster;
+import fi.paivola.mapserver.models.ExampleGlobal;
 import fi.paivola.mapserver.utils.CSVDumper;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import static java.lang.Integer.parseInt;
 import java.net.UnknownHostException;
@@ -19,7 +24,14 @@ public class App {
 
     static final boolean profilingRun = false;
 
-    public static void main(String[] args) throws UnknownHostException, IOException, ParseException, InterruptedException {
+    public static void main(String[] args) throws UnknownHostException, IOException, ParseException, InterruptedException, Exception {
+        InputStream stream = null;
+        if (args.length > 0) {
+            File file = new File(args[0]);
+            stream = new FileInputStream(file);
+        } else {
+            stream = App.class.getClassLoader().getResourceAsStream("default_testcase.csv");
+        }
 
         SettingsParser.parse();
 
@@ -53,6 +65,10 @@ public class App {
                         ws.stop();
                         runTest();
                         break mainloop;
+                    case "f":
+                        ws.stop();
+                        TestcaseRunner tr = new TestcaseRunner(stream);
+                        break mainloop;
                     case "h":
                     case "help":
                         printHelp();
@@ -69,6 +85,7 @@ public class App {
     static void printHelp() {
         System.out.println("q|e|quit|exit   - Quits the program\n"
                 + "t|test          - Run the test function\n"
+                + "f               - Run the TestcaseRunner\n"
                 + "h|help          - Display this help");
     }
 
---
+++ src/main/java/fi/paivola/mapserver/core/TestcaseRunner.java
@@ -0,0 +1,199 @@
+package fi.paivola.mapserver.core;
+
+import au.com.bytecode.opencsv.CSVReader;
+import fi.paivola.mapserver.core.setting.Setting;
+import fi.paivola.mapserver.core.setting.SettingMaster;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author juhani
+ */
+public class TestcaseRunner {
+
+    public class ModelE {
+
+        public String model;
+        public int line;
+    }
+
+    public class ModelA {
+
+        public Model model;
+        public String name;
+        public int line;
+    }
+
+    public class LinkE {
+
+        public int line1, line2, line3;
+    }
+
+    public class ParamE {
+
+        public int line;
+        public String what;
+        public String value;
+    }
+
+    public class DefparamE {
+
+        public String model;
+        public String what;
+        public String value;
+    }
+
+    private ArrayList<ModelE> modelE;
+    private ArrayList<LinkE> linkE;
+    private ArrayList<ParamE> paramE;
+    private ArrayList<DefparamE> defparamE;
+
+    public TestcaseRunner(InputStream stream) throws IOException, Exception {
+        modelE = new ArrayList<>();
+        linkE = new ArrayList<>();
+        paramE = new ArrayList<>();
+        defparamE = new ArrayList<>();
+        CSVReader reader = new CSVReader(new InputStreamReader(stream));
+        String[] nextLine;
+        int line = 1;
+        while ((nextLine = reader.readNext()) != null) {
+            onParseLine(line++, nextLine);
+        }
+        run();
+    }
+
+    private void onParseLine(int line, String[] a) throws Exception {
+        String act = null;
+        if (a.length != 0) {
+            act = a[0];
+        }
+        if (act.startsWith("#") || act.length() == 0) {
+            return;
+        }
+        switch (act) {
+            case "model":
+                onModel(line, a);
+                break;
+            case "link":
+                onLink(line, a);
+                break;
+            case "param":
+                onParam(line, a);
+                break;
+            case "defparam":
+                onDefparam(line, a);
+                break;
+            default:
+                throw new Exception("Unknown command "+act);
+        }
+    }
+
+    private void onModel(int line, String[] a) {
+        if (a.length != 2) {
+            return;
+        }
+        ModelE e = new ModelE();
+        e.line = line;
+        e.model = a[1];
+        modelE.add(e);
+    }
+
+    private void onLink(int line, String[] a) {
+        if (a.length != 4) {
+            return;
+        }
+        LinkE e = new LinkE();
+        e.line1 = Integer.parseInt(a[1]);
+        e.line2 = Integer.parseInt(a[2]);
+        e.line3 = Integer.parseInt(a[3]);
+        linkE.add(e);
+    }
+
+    private void onDefparam(int line, String[] a) {
+        if (a.length != 4) {
+            return;
+        }
+
+        DefparamE e = new DefparamE();
+        e.model = a[1];
+        e.what = a[2];
+        e.value = a[3];
+        defparamE.add(e);
+    }
+
+    private void onParam(int line, String[] a) {
+        if (a.length != 4) {
+            return;
+        }
+
+        ParamE e = new ParamE();
+        e.line = Integer.parseInt(a[1]);
+        e.what = a[2];
+        e.value = a[3];
+        paramE.add(e);
+    }
+
+    private Model resolveLineToModel(int line, ArrayList<ModelA> models) {
+        for (ModelA i : models) {
+            if (i.line == line) {
+                return i.model;
+            }
+        }
+        return null;
+    }
+
+    private void resolveParams(GameManager gm, ModelA model) throws Exception {
+        SettingMaster sm = gm.getDefaultSM(model.model.getClass());
+        for (DefparamE e : defparamE) {
+            if (e.model.equals(model.name)) {
+                sm.settings.get(e.what).setValue(e.value);
+            }
+        }
+        for (ParamE e : paramE) {
+            try {
+                if (e.line == model.line) {
+                    Setting s = sm.settings.get(e.what);
+                    if (s != null) {
+                        s.setValue(e.value);
+                    } else {
+                        throw new Exception("Fuck");
+                    }
+                }
+            } catch (Exception ex) {
+                
+            }
+        }
+        model.model.onActualUpdateSettings(sm);
+    }
+
+    private void run() throws Exception {
+        GameThread one = new GameThread((int) Math.floor(52.177457 * 20));
+        GameManager gm = one.game;
+
+        ArrayList<ModelA> models = new ArrayList<>();
+        for (ModelE e : modelE) {
+            Model a = gm.createModel(e.model);
+            ModelA aa = new ModelA();
+            aa.line = e.line;
+            aa.name = e.model;
+            aa.model = a;
+            resolveParams(gm, aa);
+            models.add(aa);
+        }
+        for (LinkE e : linkE) {
+            Model m1 = resolveLineToModel(e.line1, models);
+            Model m2 = resolveLineToModel(e.line2, models);
+            Model m3 = resolveLineToModel(e.line3, models);
+            gm.linkModelsWith(m1, m2, m3);
+        }
+
+        gm.printOnDone = 2;
+        one.start();
+    }
+}
