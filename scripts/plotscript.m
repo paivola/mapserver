6ce61c8f7e7ae71b021dd2ccad6fb6cc4434e751
UMTti <toivanenpihla@gmail.com>
2014-01-26 19:08:21 +0200

    Plotting now works

---
+++ scripts/plotscript.m
@@ -0,0 +1,18 @@
+arg_list = argv();
+filename = arg_list{1};
+file = dlmread(filename, ",", 'A2..Z999');
+
+x = 1:1:length(file(:,1));
+
+disp(file(:, 1));
+
+for i=1:length(file(1, :))
+   plot(x, file(:,i));
+   hold on
+endfor
+
+xlabel("time");
+ylabel("values");
+
+figure(1);
+print("output.png", "-dpng");
