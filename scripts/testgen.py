628dbea9f586263bfa4b332ed1ac5c07700dff8a
JuhaniImberg <juhani@imberg.fi>
2014-01-30 10:08:49 +0200

    testcase.py: template and datafile customizable

--- scripts/testgen.py
+++ scripts/testgen.py
@@ -4,31 +4,46 @@
 import csv
 import os
 import subprocess
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("template", nargs="?", type=argparse.FileType("r"),
+                        default="in/testcase.csv.template",
+                        help="template file")
+    parser.add_argument("data", nargs="?", type=argparse.FileType("r"),
+                        default="in/testcase.csv.data",
+                        help="data file to populate the template with")
+    parser.add_argument("-s", "--skip", nargs=1, type=int,
+                        help="how many lines to skip in data", default=0)
+    args = parser.parse_args()
+
     try:
         os.mkdir("out")
     except OSError:
         pass
 
     template = None
-    with open(os.path.join("in", "testcase.csv.template"), "r") as fp:
+    with args.template as fp:
         template = fp.read()
         fp.close()
 
-    with open(os.path.join("in", "testcase.csv.data"), "rb") as fp:
+    with args.data as fp:
         reader = csv.reader(fp, delimiter=",")
         for inx, row in enumerate(reader):
-            data = template
+            if inx >= args.skip[0]:
+                data = template
 
-            for inx2, vari in enumerate(row):
-                data = data.replace("$" + str(inx2), vari)
+                for inx2, vari in enumerate(row):
+                    data = data.replace("$" + str(inx2), vari)
 
-            with open(os.path.join("out", "testcase_" + str(inx) + ".csv"), "w") as fp2:
-                fp2.write(data)
-                fp2.close()
-            print "Saved " + os.path.join("out", "testcase_" + str(inx) + ".csv")
+                fname = os.path.join("out", "testcase_" + str(inx) + ".csv")
+
+                with open(fname, "w") as fp2:
+                    fp2.write(data)
+                    fp2.close()
+                print "Saved " + fname
 
         fp.close()
 
